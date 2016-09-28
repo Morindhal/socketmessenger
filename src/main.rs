033f9094c798f497279f6203028ebe5a73637011
bergman <morindhal@gmail.com>
2016-09-28 11:02:08 +0200

    Added comments for some info and a few questions to myself, things to consider.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ use ws::{connect, CloseCode};
 
 fn main()
 {
+/*
+*This is the server-part of the chat-program, currently simply echoes back what was recieved.
+*It is here that chat messages should be presented to the user.
+**/
     thread::spawn(move || {
         listen
         ("127.0.0.1:3012",
@@ -18,6 +22,11 @@ fn main()
         ).unwrap(); 
     });
 
+/*
+*This is the client-part of the chat-program, currently simply sends one message and then ends the program.
+*It is here that input should be placed into the program.
+*Should I have this in a thread seperate from the main thread as well or simply have a main loop in the main-thread?
+**/
     std::thread::sleep(std::time::Duration::from_millis(1000));
     connect("ws://127.0.0.1:3012", |out| {
         out.send("Hello WebSocket").unwrap();
